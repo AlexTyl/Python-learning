9c811d8b2f819ef458d314c5671fb90fb8b9c2df
Владислав <@Kapec>
2017-05-26 23:12:00 +0300

    Task 1.01 add function, input from keyboard

--- Taskes/Task1.01.py
+++ Taskes/Task1.01.py
@@ -1,9 +1,13 @@
 """Определить количество цифр, меньших 5, используемых при записи натурального числа N"""
 
-number = 2630298349;
-counter_of_numbers_less_then_five = 0;
-while number != 0:
-    if  number%10 < 5:
-        counter_of_numbers_less_then_five += 1;
-    number //= 10;
-print(counter_of_numbers_less_then_five);
+print("Enter the number:");
+
+def less_then_five_num_count(number):
+    counter_of_numbers_less_then_five = 0;
+    while number != 0:
+        if number % 10 < 5:
+            counter_of_numbers_less_then_five += 1;
+        number //= 10;
+    return counter_of_numbers_less_then_five;
+
+print(less_then_five_num_count(int(input())));
