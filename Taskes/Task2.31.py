3b4529686862334ab47cc1d7d318a20bb2cf1cb9
Владислав <@Kapec>
2017-06-01 23:30:56 +0300

    Correction Task2.31, Finding all the derivatives

--- Taskes/Task2.31.py
+++ Taskes/Task2.31.py
@@ -20,16 +20,17 @@ def creating_polynomial(array, x):
     while power >= 0:
         polynomial = polynomial + " + (" + str(array[index]) + " * " + str(x) + "^" + str(power) + ")"
         value_of_polynomial += array[index] * x ** power
-        derivative = derivative + " + (" + str(power) + " * " + str(array[index]) + " * " + str(x) + "^" + str(power-1) + ")"
-        values_individual_derivative.append(power * array[index] * x ** (power-1))
+        if power > 0:
+            derivative = derivative + " + (" + str(power*array[index]) + " * " + str(x) + "^" + str(
+                power - 1) + ")"
+            values_individual_derivative.append(power * array[index])
         power -= 1
         index += 1
     print(polynomial[3:len(polynomial)] + "\nThe value of a polynomial: " + str(value_of_polynomial))
-    print(derivative[3:len(derivative)])
-    index = 1
-    for i in values_individual_derivative:
-        print("Value " + str(index) + " derivative: " + str(i) + "; ", end='')
-        index += 1
+    print("Polynomial derivative: " + derivative[3:len(derivative)])
+    print("=====================================================")
+    if len(values_individual_derivative) > 0:
+        creating_polynomial(values_individual_derivative, x)
 
 print("Enter number of elements of a polynomial: ")
 number_of_elements = int(input())
@@ -42,6 +43,7 @@ array = random_array(number_of_elements, -10, 20)
 for i in array:
     print(str(i)+" ", end='')
 
-print ()
+print()
+print()
 
 creating_polynomial(array, x)
